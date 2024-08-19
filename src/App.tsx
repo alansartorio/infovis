22a4e8079eb01139aaeb511400e9ee15afc00d4a
Agustin Benvenuto <abenvenuto@itba.edu.ar>
2024-08-19 12:48:49 -0300

    initial prep

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="dark flex h-screen w-full items-center justify-center bg-background text-foreground">
+      <span className="font-bold text-muted-foreground">
+        Comming soon...{' '}
+        <span className="font-bold text-purple-400">abenve</span>
+      </span>
+    </div>
+  )
+}
+
+export default App
