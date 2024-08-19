e357aca681c8ac25fba8ac68279638f1795488f3
Agustin Benvenuto <abenvenuto@itba.edu.ar>
2024-08-19 15:01:45 -0300

    fix: typo

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="dark flex h-screen w-full items-center justify-center gap-4 bg-background text-foreground">
       <span className="font-bold text-muted-foreground">
-        Comming soon... <span className="font-bold text-red-400">abenve</span>
+        Coming soon... <span className="font-bold text-red-400">abenve</span>
       </span>
       <img
         className="inline-block h-8 w-8 border-spacing-8 rounded-full border border-border"
