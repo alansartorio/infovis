5705542c483185172fdaf0bd9b0230fc4d53ad2d
Agustin Benvenuto <abenvenuto@itba.edu.ar>
2024-08-19 12:53:22 -0300

    chore: add ab images and icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 function App() {
   return (
-    <div className="dark flex h-screen w-full items-center justify-center bg-background text-foreground">
+    <div className="dark flex h-screen w-full items-center justify-center gap-4 bg-background text-foreground">
       <span className="font-bold text-muted-foreground">
-        Comming soon...{' '}
-        <span className="font-bold text-purple-400">abenve</span>
+        Comming soon... <span className="font-bold text-red-400">abenve</span>
       </span>
+      <img
+        className="inline-block h-8 w-8 border-spacing-8 rounded-full border border-border"
+        src="./images/ab-square.png"
+        alt="abenve"
+      />
     </div>
   )
 }
